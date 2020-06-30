62d1802091695beb54661270e3168136ad2af967
Patrick Lyden <syncc12@verizon.net>
2020-06-30 19:06:49 +0000

    Added ability to view lyrics.

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import history from 'history/browser';
 import axios from 'axios';
 import ajaxPath from '../helpers/ajax';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -7,17 +8,34 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Lyrics from './Lyrics';
 
+
+function Songs(props) {
+
+  const { e, selectedArtistID, lyricsHandle } = props;
+
+  return (
+    <ListGroup.Item 
+      as="li"
+      action
+      href={`/index/artists/${selectedArtistID}/lyrics/${e['id']}`}
+      onClick={(() => lyricsHandle(`/index/artists/${selectedArtistID}/lyrics/${e['id']}`,e['id']))}
+    >
+      {`${e['song_title_korean']} - ${e['song_title_english']}`}
+    </ListGroup.Item>
+  );
+}
+
 class Index extends React.Component {
 
   constructor() {
     super();
     this.state = {
       selectedArtist: '',
-      selectedSong: '',
-      lyricsPath: '',
       artistList: [],
       lyricsList: [],
-      selectedArtistID: ''
+      selectedArtistID: '',
+      selectedSongID: '',
+      showLyrics: false
     }
   }
 
@@ -42,54 +60,77 @@ class Index extends React.Component {
     const targetArtist = e.target.textContent;
     this.setState({selectedArtist: targetArtist, selectedArtistID: artistID});
     this.getLyrics(artistID);
+    history.push(`/index/artists/${artistID}`);
   }
 
-  lyricsHandle = (inHREF, inSong) => {
-    this.setState({selectedSong: inSong,lyricsPath: inHREF});
-    console.log(this.state.selectedSong, this.state.lyricsPath);
+  lyricsHandle = (inHREF, inSongID) => {
+    this.setState({selectedSongID: inSongID,lyricsPath: inHREF, showLyrics: true});
+    history.push(inHREF);
+  }
+
+  possibleRoutes = () => {
+
   }
 
   componentDidMount() {
+    this.setState({showLyrics: false});
     this.getArtists();
   }
 
   render() {
-    const { selectedArtist, selectedSong, lyricsPath, artistList, lyricsList, selectedArtistID } = this.state;
+    const { selectedArtist, artistList, lyricsList, selectedArtistID, selectedSongID, showLyrics } = this.state;
 
     return (
       <div>
-        <br />
-        <Row>
-          <Col lg={{span: 4, offset: 1}}>
-            <ListGroup as="ul">
-              {artistList.map((e,i) => <ListGroup.Item as="li" key={i} action href={`index/${e['id']}`} onClick={((event) => this.showSongs(event,e['id']))}>{e['name']}</ListGroup.Item>)}
-            </ListGroup>
-          </Col>
-          <Col lg={{span: 4, offset: 1}}>
-            {
-              selectedArtist !== '' ?
-                <Router>
+        <Router>
+          <br />
+          {
+            showLyrics === false ?
+              <Row>
+                <Col lg={{span: 4, offset: 1}}>
                   <ListGroup as="ul">
-                    {lyricsList.map((e,i) => 
-                      <ListGroup.Item key={i} as="li" action href={`index/${selectedArtistID}/lyrics/${e['id']}`} onClick={(() => this.lyricsHandle(`index/${selectedArtistID}/lyrics/${e['id']}`,`${e['song_title_korean']} - ${e['song_title_english']}`))}>
-                        {`${e['song_title_korean']} - ${e['song_title_english']}`}
-                      </ListGroup.Item>)}
+                    {artistList.map((e,i) => <ListGroup.Item as="li" key={i} action href={`/index/artists/${e['id']}`} onClick={((event) => this.showSongs(event,e['id']))}>{e['name']}</ListGroup.Item>)}
                   </ListGroup>
-
-                  <Switch>
-                    <Route path={lyricsPath}>
-                      <Lyrics artist={selectedArtist} song={selectedSong} />
-                    </Route>
-                  </Switch>
-                </Router>
-              :
-                ''
-            }
-          </Col>
-        </Row>
+                </Col>
+                <Col lg={{span: 4, offset: 1}}>
+                  {
+                    selectedArtist !== '' ?
+                      <ListGroup as="ul">
+                        {lyricsList.map((e,i) =>
+                          <Songs key={i} e={e} selectedArtistID={selectedArtistID} lyricsHandle={this.lyricsHandle}/>
+                        )}
+                      </ListGroup>
+                    :
+                      ''
+                  }
+                </Col>
+              </Row>
+            :
+              ''
+          }
+              <Switch>
+                <Route exact path={`/index/artists/${selectedArtistID}/lyrics/${selectedSongID}`}>
+                  {
+                    showLyrics ?
+                      <Row>
+                        <Col>
+                          <Lyrics songID={selectedSongID} lyricsData={lyricsList} artistName={selectedArtist} />
+                        </Col>
+                      </Row>
+                    :
+                      ''
+                  }
+                </Route>
+              </Switch>
+        </Router>
       </div>
     )
   }
 }
 
 export default Index;
+
+
+// <ListGroup.Item key={i} as="li" action href={`/artists/${selectedArtistID}/lyrics/${e['id']}`} onClick={(() => this.lyricsHandle(`/index/${selectedArtistID}/lyrics/${e['id']}`,`${e['id']} - ${e['song_title_english']}`))}>
+//   {`${e['song_title_korean']} - ${e['song_title_english']}`}
+// </ListGroup.Item>
--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -1,14 +1,48 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Lyrics extends React.Component {
 
+  getJSONFromID = (inList, inID) => {
+    for (let i of inList) {
+      if (i['id'] === inID) {
+        return i;
+      }
+    }
+  }
+
   render() {
-    
+    const { songID, lyricsData, artistName } = this.props;
+    const lyricsJSON = this.getJSONFromID(lyricsData, songID);
 
     return (
-      <div>
-        Lyrics
-      </div>
+      <>
+        <Row>
+          <Col>
+            {lyricsJSON['song_title_korean']}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {lyricsJSON['song_title_english']}
+          </Col>
+        </Row>
+        <Row>
+          <Col> 
+            {artistName}
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col xs={{span: 10, offset: 1}} lg={{span: 4, offset: 1}}>
+            {lyricsJSON['korean_lyrics']}
+          </Col>
+          <Col xs={{span: 10, offset: 1}} lg={{span: 4, offset: 1}}>
+            {lyricsJSON['english_lyrics']}
+          </Col>
+        </Row>
+      </>
     )
   }
 }
